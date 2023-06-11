221d5d733d5c4a1cdd6e81a3accb39fa9f8f169e
fedeegiorgi <fgiorgi@mail.utdt.edu>
2023-06-11 20:43:18 -0300

    edits solution

--- src/gap_solution.cpp
+++ src/gap_solution.cpp
@@ -8,18 +8,40 @@ GapSolution::GapSolution(int n, int m) {
     this->_conjuntos = std::vector<std::vector<int>>(m, {});
 }
 
-int GapSolution::getN(){
+int GapSolution::getN() const{
     return this->_n;
 }
 
-int GapSolution::getM(){
+int GapSolution::getM() const{
     return this->_m;
+}
+
+std::vector<std::vector<int>> GapSolution::getConj() const{
+    return this->_conjuntos;
 }
 
 void GapSolution::assign(int vendedor, int deposito) {
     this->_conjuntos[deposito].push_back(vendedor);
 }
 
+void printVector(const std::vector<int>& vec) {
+    std::cout << "[";
+    for (size_t i = 0; i < vec.size(); ++i) {
+        std::cout << vec[i];
+        if (i != vec.size() - 1) {
+            std::cout << ", ";
+        }
+    }
+    std::cout << "]" << std::endl;
+}
+
 std::ostream& operator<<(std::ostream& os, const GapSolution& solution) {
+    os << "depositos: ";
+    for (int i = 0; i < solution.getM(); i++)
+        os << i << " ";
+    os << std::endl;
+    for (int i = 0; i < solution.getM(); i++) {
+        printVector(solution.getConj()[i]);
+    }
     return os;
 }
--- src/gap_solution.h
+++ src/gap_solution.h
@@ -9,8 +9,9 @@ class GapSolution
     public:
     GapSolution();
     GapSolution(int n, int m);
-    int GapSolution::getN();
-    int GapSolution::getM();
+    int GapSolution::getN() const;
+    int GapSolution::getM() const;
+    std::vector<std::vector<int>> GapSolution::getConj() const;
     void assign(int vendedor, int deposito);
 
     friend std::ostream& operator<<(std::ostream& os, const GapSolution& solution);
