5805aed62b6ce827ea34b412af8bc78558df511f
fedeegiorgi <fgiorgi@mail.utdt.edu>
2023-06-11 20:26:01 -0300

    gap solution edit

--- src/gap_solution.cpp
+++ src/gap_solution.cpp
@@ -3,7 +3,21 @@
 GapSolution::GapSolution() {}
 
 GapSolution::GapSolution(int n, int m) {
-    return;
+    this->_n = n;
+    this->_m = m;
+    this->conjuntos = std::vector<std::vector<int>>(m, {});
+}
+
+int GapSolution::getN(){
+    return this->_n;
+}
+
+int GapSolution::getM(){
+    return this->_m;
+}
+
+void GapSolution::assign(int vendedor, int deposito) {
+    this->conjuntos[deposito].push_back(vendedor);
 }
 
 std::ostream& operator<<(std::ostream& os, const GapSolution& solution) {
--- src/gap_solution.h
+++ src/gap_solution.h
@@ -9,9 +9,16 @@ class GapSolution
     public:
     GapSolution();
     GapSolution(int n, int m);
+    int GapSolution::getN();
+    int GapSolution::getM();
+    void assign(int vendedor, int deposito);
+
     friend std::ostream& operator<<(std::ostream& os, const GapSolution& solution);
 
     private:
+    int _n;
+    int _m;
+    std::vector<std::vector<int>> conjuntos;
 };
 
 #endif
