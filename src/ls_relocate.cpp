cb377a3a6510467d413e77ba1926b4bf72cf9ca7
fedeegiorgi <fgiorgi@mail.utdt.edu>
2023-07-06 21:21:25 -0300

    relocate con deposito extra

--- src/ls_relocate.cpp
+++ src/ls_relocate.cpp
@@ -9,16 +9,29 @@ lsRelocateOperator::lsRelocateOperator(GapSolution &feasibleSol, GapInstance &in
 
 lsRelocateNeighbour lsRelocateOperator::getBestNeighbour() {
     lsRelocateNeighbour bestNeighbour(0, -1, -1, -1);
-
-    for (int d_i = 0; d_i < _solution.getM() - 1; d_i++) {
+    double valor_obj_Relocate = 0.0;
+    for (int d_i = 0; d_i < _solution.getM() + 1; d_i++) {
         for (int c_j = 0; c_j < _solution.getDeposit(d_i).size(); c_j++) {
             int cliente = _solution.getDeposit(d_i)[c_j];
-            for (int d_ins = 0; d_ins < _solution.getM(); d_ins++) {
+            for (int d_ins = 0; d_ins < _solution.getM()+1; d_ins++) {
                 if (d_i == d_ins) {
-                    d_ins++;
+                    continue;
                 }
 
-                double valor_obj_Relocate = _objective_value - _instance.costs[d_i][cliente] + _instance.costs[d_ins][cliente];
+                if (d_i == _solution.getM()){
+                    valor_obj_Relocate = _objective_value - _instance.maxCost*3 + _instance.costs[d_ins][cliente];
+                }
+                else{
+                    if (d_ins == _solution.getM()){
+                        valor_obj_Relocate = _objective_value - _instance.costs[d_i][cliente] + _instance.maxCost*3;
+                    }
+
+                    else{
+                        valor_obj_Relocate = _objective_value - _instance.costs[d_i][cliente] + _instance.costs[d_ins][cliente];
+                    }
+                }
+
+                
 
                 if (isRelocateFeasible(d_ins, cliente)) {
                     double delta = valor_obj_Relocate - _objective_value;
@@ -34,5 +47,11 @@ lsRelocateNeighbour lsRelocateOperator::getBestNeighbour() {
 }
 
 bool lsRelocateOperator::isRelocateFeasible(int d_ins, int cliente) {
-    return _solution.getCurrentCapacities()[d_ins] - _instance.demands[d_ins][cliente] >= 0;
+    if (d_ins == _solution.getM()){
+        return true;
+    }
+    else{
+        return _solution.getCurrentCapacities()[d_ins] - _instance.demands[d_ins][cliente] >= 0;
+    }
+
 }
