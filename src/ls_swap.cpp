af7f2aead61e4c0d5184cc0033ac8aafdd582014
fedeegiorgi <fgiorgi@mail.utdt.edu>
2023-07-06 21:21:41 -0300

    swap con deposito extra

--- src/ls_swap.cpp
+++ src/ls_swap.cpp
@@ -9,23 +9,29 @@ lsSwapOperator::lsSwapOperator(GapSolution &feasibleSol, GapInstance &instance) 
 
 lsSwapNeighbour lsSwapOperator::getBestNeighbour() {
     int dpivot = 0; // deposito pivote
+    double valor_obj_swap = 0.0;
     lsSwapNeighbour bestNeighbour(0, -1, -1, -1, -1);
 
-    while (dpivot < _solution.getM()-1){
-
+    while (dpivot < _solution.getM()){
         for (int vpivot = 0; vpivot < _solution.getDeposits()[dpivot].size(); vpivot++){ // posición de vendedor dentro del deposito pivote.
 
             int dcomp = dpivot+1; // deposito a comparar contra pivote.
 
-            while(dcomp < _solution.getM()){ 
+            while(dcomp < _solution.getM()+1){ 
                 
                 for(int vcomp = 0; vcomp < _solution.getDeposit(dcomp).size(); vcomp++){ // posición del vendedor dentro del deposito a comparar.
 
                     int vend_izq = _solution.getDeposit(dpivot)[vpivot];
                     int vend_der = _solution.getDeposit(dcomp)[vcomp];
 
-                    double valor_obj_swap = _objective_value - _instance.costs[dpivot][vend_izq] - _instance.costs[dcomp][vend_der] 
+                    if (dcomp == _solution.getM()){
+                        valor_obj_swap = _objective_value - _instance.costs[dpivot][vend_izq] - _instance.maxCost*3 
+                                            + _instance.costs[dpivot][vend_der] + _instance.maxCost*3;
+                    }
+                    else{
+                        valor_obj_swap = _objective_value - _instance.costs[dpivot][vend_izq] - _instance.costs[dcomp][vend_der] 
                                             + _instance.costs[dpivot][vend_der] + _instance.costs[dcomp][vend_izq];
+                    }
 
                     if (isSwapFeasible(dpivot, dcomp, vend_izq, vend_der)) {
                         double delta = valor_obj_swap - _objective_value;
@@ -45,8 +51,18 @@ lsSwapNeighbour lsSwapOperator::getBestNeighbour() {
 }
 
 bool lsSwapOperator::isSwapFeasible(int dpivot, int dcomp, int vend_izq, int vend_der) {
-    bool deposit_izq = _solution.getCurrentCapacities()[dpivot] + _instance.demands[dpivot][vend_izq] - _instance.demands[dpivot][vend_der] >= 0;
-    bool deposit_der = _solution.getCurrentCapacities()[dcomp] + _instance.demands[dcomp][vend_der] - _instance.demands[dcomp][vend_izq] >= 0;
+    bool deposit_izq = true;
+    bool deposit_der = true;
+    
+    if (dcomp == _solution.getM()){
+        deposit_izq = _solution.getCurrentCapacities()[dpivot] + _instance.demands[dpivot][vend_izq] - _instance.demands[dpivot][vend_der] >= 0;
+        deposit_der = true;
+    }
+
+    else{
+        deposit_izq = _solution.getCurrentCapacities()[dpivot] + _instance.demands[dpivot][vend_izq] - _instance.demands[dpivot][vend_der] >= 0;
+        deposit_der = _solution.getCurrentCapacities()[dcomp] + _instance.demands[dcomp][vend_der] - _instance.demands[dcomp][vend_izq] >= 0;
+    }
 
     return deposit_izq && deposit_der;
 }
